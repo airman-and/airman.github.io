e03ca90fdcfa3af6c3b7ab7a899b1df2ea4e0ca5
Andy Cho <a01064943103@gmail.com>
2025-09-19 21:12:37 +0900

    chore: update skills to Python, C, VIbeCoding

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -61,20 +61,9 @@ const CONFIG = {
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Python',
+    'C',
+    'VIbeCoding',
   ],
   experiences: [
     {
